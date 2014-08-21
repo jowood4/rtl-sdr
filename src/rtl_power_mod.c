7b5f45fa97aea4e0caeb7c6c1e02ff2b2c5840b9
Joe <jowood4@gmail.com>
2014-08-21 11:55:17 -0400

    Added DC offset to RMS calculation

--- src/rtl_power_mod.c
+++ src/rtl_power_mod.c
@@ -181,7 +181,7 @@ void rms_power(int ts_index, uint8_t *buf, double *rms_pow_val, double *rms_pow_
 
 	rms_sum = 0;
 	dc_sum = 0;
-	dc_offset = 0;
+	dc_offset_sum = 0;
 
 	for (i=0; i<num_samples; i=i+1) {
 		dc_offset_sum = dc_offset_sum + (int)buf[i];
